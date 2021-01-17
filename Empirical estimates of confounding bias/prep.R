19680ab6c45970b167496a46d9431e5eeee14fbb
Maya Mathur <mmathur@stanford.edu>
2021-01-17 06:25:47 -0800

    init

---
+++ Empirical estimates of confounding bias/prep.R
@@ -0,0 +1,35 @@
+
+
+########################### PRELIMINARIES ########################### 
+
+library(dplyr)
+library(readxl)
+library(robumeta)
+library(Hmisc)
+library(data.table)
+library(testthat)
+library(here)
+
+raw.data.dir = here("Raw data")
+prepped.data.dir = here("Prepped data")
+code.dir = here()
+
+
+########################### BUN 2020 ########################### 
+
+# can't scrape because they don't have study-level data
+# @try emailing them
+
+########################### GOLDER 2011 ########################### 
+
+
+
+########################### SHIKATA 2006 ########################### 
+
+########################### IOANNIDIS 2001 ########################### 
+
+
+
+
+
+########################### FRANKLIN 2020 (RCT-DUPLICATE) ########################### 
